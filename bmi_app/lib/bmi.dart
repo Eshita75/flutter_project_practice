084568c3b03ff620256b21d67cff1b8083cdbccb
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-05-21 23:23:55 +0600

    success

---
+++ bmi_app/lib/bmi.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class bmiCalculator extends StatelessWidget {
+  const bmiCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
