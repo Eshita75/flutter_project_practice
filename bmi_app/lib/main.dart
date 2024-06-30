45a81063efbe2ee9e29e6c73c1d9a41f1f250bbb
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-06-30 09:31:13 +0600

    success

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_app/bmi.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      debugShowCheckedModeBanner: false,
+      home: bmiCalculator(),
     );
   }
 }
