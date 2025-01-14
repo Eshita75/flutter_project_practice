7f73c0f7a36dba87580c1146a683c5fda245fa37
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2025-01-14 12:04:41 +0600

    success

---
+++ grocery_store_app_ui_design/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:grocery_store_app_ui_design/app.dart';
+
+main(){
+  runApp(const GroceryApp());
+}
+
